8dc42729706e8fa43f8777100ec2e2e22fd43630
tuttlebp <tuttlebp@rose-hulman.edu>
2019-03-11 01:20:19 -0400

    My lines are backwards, but you said that was okay

--- src/m3_capital_t_class.py
+++ src/m3_capital_t_class.py
@@ -3,8 +3,8 @@ A   CapitalT   class and functions that use/test it.
 
 Authors: David Mutchler, Vibha Alangar, Matt Boutell, Dave Fisher,
          Mark Hays, Amanda Stouder, Aaron Wilkin, their colleagues,
-         and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         and Brett Tuttle.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 import rosegraphics as rg
 
@@ -19,11 +19,10 @@ def main():
     # -------------------------------------------------------------------------
     print('Un-comment the calls in MAIN one by one')
     print(' to run the testing code as you complete the TODOs.')
-
-    # run_test_simple_t()
-    # run_test_set_colors()
-    # run_test_move_by()
-    # run_test_clone()
+    run_test_simple_t()
+    run_test_set_colors()
+    run_test_move_by()
+    run_test_clone()
 
 
 def run_test_simple_t():
@@ -162,7 +161,7 @@ class CapitalT(object):
           :type letter_thickness:    int
         """
         # ---------------------------------------------------------------------
-        # TODO: 3.
+        # DONE: 3.
         #   READ the above specification, including the Example.
         #   Implement this method, using the instance variables
         #      h_rect
@@ -170,6 +169,16 @@ class CapitalT(object):
         #   and *** NO OTHER INSTANCE VARIABLES. ***
         #   Note: Implement   attach_to   before testing this __init__ method.
         # ---------------------------------------------------------------------
+        start_h_point = rg.Point(intersection_center.x - width / 2, intersection_center.y - letter_thickness / 2)
+        end_h_point = rg.Point(intersection_center.x + width / 2, intersection_center.y + letter_thickness / 2)
+        h_rect = rg.Rectangle(start_h_point, end_h_point)
+        start_v_point = rg.Point(intersection_center.x - letter_thickness / 2,
+                                 intersection_center.y - letter_thickness / 2)
+        end_v_point = rg.Point(intersection_center.x + letter_thickness / 2,
+                               intersection_center.y + height - letter_thickness / 2)
+        v_rect = rg.Rectangle(start_v_point, end_v_point)
+        self.h_rect = h_rect
+        self.v_rect = v_rect
 
     def attach_to(self, window):
         """
@@ -190,7 +199,7 @@ class CapitalT(object):
           :type window: rg.RoseWindow
         """
         # ---------------------------------------------------------------------
-        # TODO: 4.
+        # DONE: 4.
         #   READ the above specification, including the Example.
         #   Implement this method, then TEST it by:
         #     a. Un-comment the call to its test function, in main.  Run.
@@ -198,6 +207,8 @@ class CapitalT(object):
         #     c. Compare the graphics window to the   simple_t.pdf   pictures.
         #        They should look exactly the same as each other.
         # ---------------------------------------------------------------------
+        self.h_rect.attach_to(window)
+        self.v_rect.attach_to(window)
 
     def set_colors(self, fill_color, outline_color):
         """
@@ -222,7 +233,7 @@ class CapitalT(object):
           :type outline_color: str
         """
         # ---------------------------------------------------------------------
-        # TODO: 5.
+        # DONE: 5.
         #   READ the above specification, including the Example.
         #   Implement this method, then TEST it by:
         #     a. Un-comment the call to its test function, in main.  Run.
@@ -230,6 +241,11 @@ class CapitalT(object):
         #     c. Compare the graphics window to the  set_colors.pdf   pictures.
         #        They should look exactly the same as each other.
         # ---------------------------------------------------------------------
+        self.v_rect.fill_color = fill_color
+        self.h_rect.fill_color = self.v_rect.fill_color
+
+        self.v_rect.outline_color = outline_color
+        self.h_rect.outline_color = self.v_rect.outline_color
 
     def move_by(self, dx, dy):
         """
@@ -266,6 +282,15 @@ class CapitalT(object):
         #        Note: the pdf shows the different locations that
         #        the T moves through, but there is only one T at any moment.
         # ---------------------------------------------------------------------
+        self.h_rect.corner_1.x = self.h_rect.corner_1.x + dx
+        self.h_rect.corner_1.y = self.h_rect.corner_1.y + dy
+        self.v_rect.corner_1.x = self.v_rect.corner_1.x + dx
+        self.v_rect.corner_1.y = self.v_rect.corner_1.y + dy
+
+        self.h_rect.corner_2.x = self.h_rect.corner_2.x + dx
+        self.h_rect.corner_2.y = self.h_rect.corner_2.y + dy
+        self.v_rect.corner_2.x = self.v_rect.corner_2.x + dx
+        self.v_rect.corner_2.y = self.v_rect.corner_2.y + dy
 
     def clone(self):
         """
@@ -300,8 +325,19 @@ class CapitalT(object):
         # IMPORTANT RESTRICTION: You are NOT permitted to add any instance
         # variables beyond  h_rect  and  v_rect, at any point of this exercise.
         #######################################################################
+        x_center = (self.h_rect.corner_1.x + self.h_rect.corner_2.x) / 2
+        y_center = (self.h_rect.corner_1.y - self.h_rect.corner_2.y) / 2
 
+        center = rg.Point(x_center, y_center)
 
+        the_t = CapitalT(center, self.h_rect.corner_2.x - self.h_rect.corner_1.x,
+                         self.v_rect.corner_2.y - self.v_rect.corner_1.y, self.v_rect.corner_2.x - self.v_rect.corner_1.x)
+        the_t.v_rect.fill_color = self.v_rect.fill_color
+        the_t.h_rect.fill_color = self.h_rect.fill_color
+        the_t.v_rect.outline_color = self.v_rect.outline_color
+        the_t.h_rect.outline_color = self.h_rect.outline_color
+
+        return the_t
 # -----------------------------------------------------------------------------
 # Calls  main  to start the ball rolling.
 # -----------------------------------------------------------------------------
